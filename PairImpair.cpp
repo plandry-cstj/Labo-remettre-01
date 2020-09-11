c8cd0b4c64a1510759091209955dbd6589ff555a
plandry-cstj <70448324+plandry-cstj@users.noreply.github.com>
2020-09-11 17:52:21 -0400

    Add files via upload

---
+++ PairImpair.cpp
@@ -0,0 +1,39 @@
+// But : Écrire un programme qui lit un entier et qui détermine et affiche s’il s’agit d’un entier pair ou d’un entier impair.
+// Auteur : Pierre-Olivier Landry
+// Date : 2020-09-11
+
+#include <iostream>
+
+void main()
+{
+
+	setlocale(LC_ALL, "");
+
+	int nombre;
+
+	// Le programme affiche un message qui demande à l'utilisateur d'écrire un nombre
+	std::cout << "Veuillez entrer un nombre : ";
+	//Le programme lit le nombre écrit par l'utilisateur
+	std::cin >> nombre;
+
+	// Le programme calcule le nombre et indique s'il est pair ou impair
+	if (nombre % 2 == 0)
+	{
+		std::cout << nombre << " est un nombre pair";
+	}
+	else
+	{
+		std::cout << nombre << " est un nombre impair";
+	}
+
+}
+
+// Plan de test
+/*
+nombre						Résultat escompté
+
+204							204 est un nombre pair
+15							15 est un nombre impair
+25							26 est un nombre impair
+30							30 est un nombre pair
+*/
---
+++ Vertical.cpp
@@ -0,0 +1,57 @@
+// But : Écrire un programme qui prend un nombre de cinq chiffres. Il vérifie que le nombre a bien 5 chiffres et les affiche verticalement
+// Auteur : Pierre-Olivier Landry
+// Date : 2020-09-11
+
+
+#include <iostream>
+
+void main()
+{
+
+	setlocale(LC_ALL, "");
+
+	int nombre;
+
+	//Le programme demande à l'utilisateur d'entrer un nombre à 5 chiffres
+	std::cout << "Veuillez entrer un nombre à 5 chiffres : ";
+	// Le nombre que l'utilisateur a écrit est lu par le programme
+	std::cin >> nombre;
+	
+	// Le programme prends le premier chiffre et le stock
+	chiffre1 = nombre / 10000;
+
+
+
+	if (nombre > 9999 && nombre < 100000)
+	{
+		// Le programme affiche le premier chiffre
+		std::cout << chiffre1<<std::endl;
+	}
+	else
+	{
+		std::cout << "Ce nombre ne contient pas 5 chiffres.";
+	}
+
+}
+
+
+// Plan de test
+/*
+Nombre						Résultat escompté
+
+12345						1
+							2
+							3
+							4
+							5
+
+31574						3
+							1
+							5
+							7
+							4
+
+12							Ce nombre ne contient pas 5 chiffres
+
+3745						Ce nombre ne contient pas 5 chiffres
+*/
